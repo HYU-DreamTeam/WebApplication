558644eed0121a727c82f4fa38aa23ff95fc4faf
junseowon <junseowon@gmail.com>
2025-12-30 23:17:16 +0900

    feat: turn Card
    템플릿은 아직이고 귀찮아서 그냥 앞면이 빨간색, 뒷면이 초록임 버그 있으면 말 좀 부탁.

--- js/card.js
+++ js/card.js
@@ -2,8 +2,17 @@ const modal = document.getElementById('modal');
 const hBtn = document.getElementById('horizontal-btn');
 const vBtn = document.getElementById('vertical-btn');
 const canvas = document.getElementById('canvas');
-const toolBtn = document.getElementById('tool-btn');
+const templateBtn = document.getElementById('template-btn');
+const template = document.getElementById('template');
 const tool = document.getElementById('tool');
+const canvasTrunBtn = document.getElementById('turn-canvas-btn');
+
+const newCard = document.createElement('div');
+const cardFront = document.createElement('div');
+const cardBack = document.createElement('div');
+
+let activateTool = false;
+let trunCard = false;
 
 // 페이지 로드 시 팝업 보이기
 window.onload = function() {
@@ -12,34 +21,52 @@ window.onload = function() {
 
 // canvas에 card 만들기
 function createCard(type) {
-
-    const newCard = document.createElement('div');
-
     newCard.classList.add('card');
 
     if(type === 'horizontal'){
-        newCard.classList.add('card-horizontal');
+        newCard.classList.add('card-'+type);        
+        cardFront.classList.add('card-'+type+'-front');
+        cardBack.classList.add('card-'+type+'-back');
     }
     else{
-        newCard.classList.add('card-vertical');
+        newCard.classList.add('card-'+type);        
+        cardFront.classList.add('card-'+type+'-front');
+        cardBack.classList.add('card-'+type+'-back');
     }
 
+    newCard.appendChild(cardFront);
+    newCard.appendChild(cardBack);
     canvas.appendChild(newCard);
+    
     modal.style.display = 'none';
 }
 
 // 버튼 클릭 시 크기 정의 후, 팝업 숨기기
 hBtn.onclick = function() {
-    createCard('horizontal');
     createCard('horizontal');    
 };
 
 vBtn.onclick = function() {
-    createCard('vertical');
-    createCard('vertical');
+    createCard('vertical');    
 };
 
-toolBtn.onclick = function() {
-    tool.style.width = '300px';
-    canvas.style.marginLeft = "300px";
+templateBtn.onclick = function() {
+
+    activateTool = !activateTool;
+
+    const templateWidthSize = activateTool ? 400 : 0;
+    const canvasMarginLeftSize = activateTool ? tool.offsetWidth + templateWidthSize : 0;
+
+    template.style.width = templateWidthSize + 'px';
+    canvas.style.marginLeft = canvasMarginLeftSize + 'px';    
 };
+
+canvasTrunBtn.onclick = function(){    
+    trunCard = !trunCard;
+
+    const frontDeg = trunCard ? '-180deg' : '0deg';
+    const backDeg = trunCard ? '0deg' : '180deg';
+
+    cardFront.style.transform = 'rotateY(' + frontDeg + ')';
+    cardBack.style.transform = 'rotateY(' + backDeg + ')';        
+}
