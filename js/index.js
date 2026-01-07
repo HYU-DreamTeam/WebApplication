422483b6634a32cc7a94f94f54ae8bf8b35e416a
JH <jhdb96@gmail.com>
2026-01-07 21:40:16 +0900

    어떻게 하는거지

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// JS
+var heroText = document.getElementById("heroText");
+
+setTimeout(function () {
+  heroText.className = "show";
+}, 100);
+
+setTimeout(function () {
+  var strong = heroText.getElementsByTagName("strong")[0];
+  strong.style.color = "#5b6cff";
+}, 800);
